290a08913d386bdc8c64a6f47937d675da5c77cb
Dipali Bhandurge <bhandurgedipali5@gmail.com>
2026-07-26 20:18:33 +0530

    add new code(typecasting)

--- code.py
+++ code.py
@@ -1,8 +1,5 @@
-num1=(int(input("enter 1st number")))
-num2=(int(input("enter 2nd number")))
+#typecasting
 
-add= num1+num2
-print(add)
-
-average= add/2
-print(average)
+a="3"
+b="4"
+print(int(a) + int(b))
